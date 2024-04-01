6b54314071879ef6a3a9da62914340bd4b9782be
Natalya Ivanova <natalya.nata-cub@yandex.ru>
2024-04-02 01:13:08 +0300

    added task3

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Задайте произвольный массив. Выведете 
+// его элементы, начиная с конца. Использовать 
+// рекурсию, не использовать циклы
+// [1,1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+using System.Xml.XPath;
+
+void PrintArray(int[]arr, int i =  0)
+{
+    if (i < arr.Length)
+    {
+         PrintArray(arr, i + 1);
+        Console.Write( $" { arr[i]} ");
+        return ;
+    }
+}
+int[] arr = {1, 2, 5, 0, 10, 34};
+int length = arr.Length;
+
+PrintArray(arr);
